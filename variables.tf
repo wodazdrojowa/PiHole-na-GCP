cb192d0be62603cb8f0cbac902965d811193d1b3
wodazdrojowa <wodazdrojowa@gmail.com>
2026-02-23 01:10:51 +0000

    .

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "ssh_user" {
 variable "ssh_public_key_path" {
   description = "Path to SSH public key"
   type        = string
-  default     = "~/.ssh/id_rsa.pub"
+  default     = "/mnt/workspace/id_ed25519.pub"
 }
 
 variable "zone" {
