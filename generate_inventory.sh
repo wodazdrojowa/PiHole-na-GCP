e19ae0e12437887cfd9efe53f7be2a6d8f39aa9a
wodazdrojowa <wodazdrojowa@gmail.com>
2026-03-27 22:40:08 +0100

    Update generate_inventory.sh

--- generate_inventory.sh
+++ generate_inventory.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 cat > /mnt/workspace/source/inventory.py << EOF
 [pihole-server]
-pihole-server ansible_host=${TF_OUTPUT_server_ip} ansible_user=ansible ansible_ssh_private_key_file=/mnt/workspace/id_ed25519
+
+#pihole-server ansible_host=${TF_OUTPUT_server_ip} ansible_user=ansible ansible_ssh_private_key_file=/mnt/workspace/id_ed25519
+pihole-server ansible_host=os.environ.get("INSTANCE_IP") ansible_user=ansible ansible_ssh_private_key_file=/mnt/workspace/id_ed25519
 
 [pihole-server:vars]
 ansible_python_interpreter=/usr/bin/python3
