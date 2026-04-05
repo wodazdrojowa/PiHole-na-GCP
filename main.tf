da7fe364f16a763a997d7d8121c05f8d9029b872
wodazdrojowa <wodazdrojowa@gmail.com>
2026-04-05 22:56:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,10 +82,10 @@ resource "google_compute_firewall" "pihole_wireguard" {
   target_tags   = ["pihole"]
 }
 
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
+#resource "tls_private_key" "ssh_key" {
+#  algorithm = "RSA"
+#  rsa_bits  = 4096
+#}
 
 # Compute instance
 resource "google_compute_instance" "pihole" {
@@ -136,21 +136,22 @@ output "instance_name" {
   description = "Name of the instance"
 }
 
-output "ssh_public_key" {
-  value       = tls_private_key.ssh_key.public_key_openssh
-  description = "Public SSH key for Pi-hole instance"
-}
+#output "ssh_public_key" {
+#  value       = tls_private_key.ssh_key.public_key_openssh
+#  description = "Public SSH key for Pi-hole instance"
+#}
 
-output "ssh_private_key" {
-  value       = tls_private_key.ssh_key.private_key_pem
-  sensitive   = true
-  description = "Private SSH key - use to connect via SSH"
-}
+#output "ssh_private_key" {
+#  value       = tls_private_key.ssh_key.private_key_pem
+#  sensitive   = true
+#  description = "Private SSH key - use to connect via SSH"
+#}
 
 resource "local_file" "ansible_inventory" {
   content = <<-EOF
     [pihole-server]
-    pihole-server ansible_host=${google_compute_address.pihole_ip.address} ansible_user=ansible ansible_ssh_private_key_file=/mnt/workspace/ssh_key/ssh_public
+#    pihole-server ansible_host=${google_compute_address.pihole_ip.address} ansible_user=ansible ansible_ssh_private_key_file=/mnt/workspace/ssh_key/ssh_public
+   pihole-server ansible_host=${google_compute_address.pihole_ip.address} ansible_user=
 
     [pihole-server:vars]
     ansible_python_interpreter=/usr/bin/python3
