14a7b2ee284f366676332f381e4e526ecbeea2c7
wodazdrojowa <wodazdrojowa@gmail.com>
2026-04-19 00:11:46 +0200

    Add outputs for SSH private key and OS login user

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "ssh_private_key" {
+  value     = tls_private_key.ansible_ssh.private_key_openssh
+  sensitive = true
+}
+
+output "os_login_user" {
+  value = google_os_login_ssh_public_key.ansible.user
+}
