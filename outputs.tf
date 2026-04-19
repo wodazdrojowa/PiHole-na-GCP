15f37ec7505bc37f5ad55bc1a733a1f50a474a5c
wodazdrojowa <wodazdrojowa@gmail.com>
2026-04-19 11:56:53 +0200

    Uncomment os_login_user output in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,6 @@ output "ssh_private_key" {
   sensitive = true
 }
 
-#output "os_login_user" {
-#  value = google_os_login_ssh_public_key.ansible.user
-#}
+output "os_login_user" {
+  value = google_os_login_ssh_public_key.ansible.user
+}
