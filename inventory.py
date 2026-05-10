fbb60731889c227020f4874095e711942b0bb9d5
wodazdrojowa <wodazdrojowa@gmail.com>
2026-05-10 21:54:24 +0200

    Clean up inventory.py by removing comments
    Removed commented-out code for server IP and user settings.

--- inventory.py
+++ inventory.py
@@ -5,12 +5,6 @@ import sys
 
 def main():
 ip = os.environ.get('INSTANCE_IP', '')
-#server_ip = os.environ.get('TF_OUTPUT_server_ip', '')
-
-    #if not server_ip:
-    #    sys.stderr.write("ERROR: TF_OUTPUT_server_ip environment variable is not set\n")
-    #    sys.exit(1)
-
     inventory = {
         "pihole_server_group": {
             "hosts": ["pihole-server"],
@@ -19,9 +13,7 @@ ip = os.environ.get('INSTANCE_IP', '')
             "hostvars": {
                 "pihole-server": {
                     "ansible_host": ip,
-#                    "ansible_host": os.environ.get("INSTANCE_IP")
                     "ansible_user": os.environ.get('SSH_USER', 'ansible'),
- #                   "ansible_user": "ansible",
                     "ansible_ssh_private_key_file": "/mnt/workspace/id_ed25519",
                     "ansible_python_interpreter": "/usr/bin/python3",
                     "ansible_ssh_common_args": "-o StrictHostKeyChecking=no"
@@ -33,7 +25,6 @@ ip = os.environ.get('INSTANCE_IP', '')
         }
     }
 
-#    print(json.dumps(inventory, indent=2))
     print(json.dumps(inventory))
 
 if __name__ == '__main__':
