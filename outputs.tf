f0060447f37af3ba2f1ee6c39698912b22702399
wodazdrojowa <wodazdrojowa@gmail.com>
2026-07-21 23:59:27 +0200

    .

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,9 @@
+output "ssh_private_key" {
+  value     = tls_private_key.ansible_ssh.private_key_openssh
+  sensitive = true
+}
 
+output "domain_name" {
+  value       = var.domain_name
+  description = "Subdomain for Pi-hole"
+}
