41816c780ed78b7cd731073e410f4ecc766bb4b8
wodazdrojowa <wodazdrojowa@gmail.com>
2026-08-02 22:47:29 +0200

    auto odnawianie cert, DNS challenge OVH, i https z LetsEncrypt

--- main.tf
+++ main.tf
@@ -75,7 +75,6 @@ resource "google_compute_firewall" "pihole_wireguard" {
   allow {
     protocol = "udp"
     ports    = ["443"]
-    #zmieniłem domyślny port z 51280 na 443 bo coś operator komórkowy blokował wysokie porty
   }
 
   source_ranges = ["0.0.0.0/0"]
